df857846be1f5bb8d4e2ee04d6ff00405c90c419
katamartin <katasm@gmail.com>
2022-08-05 10:05:53 -0700

    Fetch data manually in page

--- demo/pages/test-leap.js
+++ demo/pages/test-leap.js
@@ -1,4 +1,5 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
+import zarr from 'zarr-js'
 import { Minimap, Path, Sphere, Raster } from '@carbonplan/minimaps'
 import {
   naturalEarth1,
@@ -9,7 +10,6 @@ import {
 import { useThemeUI, Box, Flex } from 'theme-ui'
 import { Toggle, Select } from '@carbonplan/components'
 import { useThemedColormap } from '@carbonplan/colormaps'
-import { datasets } from '../datasets'
 
 const projections = {
   naturalEarth1,
@@ -18,14 +18,51 @@ const projections = {
   equirectangular,
 }
 
+const DATASET =
+  'https://cmip6downscaling.blob.core.windows.net/vis/article/fig1/regions/central-america/gcm-tasmax.zarr'
+const VARIABLE = 'tasmax'
+const NAN = 9.969209968386869e36
+
+const fetchData = () => {
+  return new Promise((resolve) =>
+    zarr().loadGroup(
+      DATASET,
+      (error, { [VARIABLE]: data, lat, lon }) => {
+        const bounds = {
+          lat: [
+            lat.data.reduce((a, b) => Math.min(a, b)),
+            lat.data.reduce((a, b) => Math.max(a, b)),
+          ],
+          lon: [
+            lon.data.reduce((a, b) => Math.min(a, b)),
+            lon.data.reduce((a, b) => Math.max(a, b)),
+          ],
+        }
+
+        resolve({ data, bounds })
+      },
+      [VARIABLE, 'lat', 'lon']
+    )
+  )
+}
+
 const Test = () => {
   const { theme } = useThemeUI()
+  const colormap = useThemedColormap('cool', { count: 255, format: 'rgb' })
+  const [data, setData] = useState()
+  const [bounds, setBounds] = useState()
   const [projection, setProjection] = useState('naturalEarth1')
   const [basemaps, setBasemaps] = useState({
     land: true,
     ocean: false,
   })
-  const colormap = useThemedColormap('cool', { count: 255, format: 'rgb' })
+
+  useEffect(() => {
+    fetchData().then((result) => {
+      setData(result.data)
+      setBounds(result.bounds)
+    })
+  }, [])
 
   return (
     <>
@@ -55,43 +92,46 @@ const Test = () => {
       </Box>
 
       <Box sx={{ width: '50%', ml: [4], mt: [6], mb: [3] }}>
-        <Minimap
-          projection={projections[projection]}
-          scale={1}
-          translate={[0, 0]}
-        >
-          {basemaps.ocean && (
-            <Path
-              fill={theme.colors.background}
-              opacity={1}
-              source={
-                'https://storage.googleapis.com/carbonplan-maps/world-atlas/ocean-50m.json'
-              }
-              feature={'ocean'}
-            />
-          )}
+        {data && bounds && (
+          <Minimap
+            projection={projections[projection]}
+            scale={1}
+            translate={[0, 0]}
+          >
+            {basemaps.ocean && (
+              <Path
+                fill={theme.colors.background}
+                opacity={1}
+                source={
+                  'https://storage.googleapis.com/carbonplan-maps/world-atlas/ocean-50m.json'
+                }
+                feature={'ocean'}
+              />
+            )}
 
-          {basemaps.land && (
-            <Path
-              stroke={theme.colors.primary}
-              source={
-                'https://cdn.jsdelivr.net/npm/world-atlas@2/land-50m.json'
-              }
-              feature={'land'}
-              opacity={1}
-            />
-          )}
+            {basemaps.land && (
+              <Path
+                stroke={theme.colors.primary}
+                source={
+                  'https://cdn.jsdelivr.net/npm/world-atlas@2/land-50m.json'
+                }
+                feature={'land'}
+                opacity={1}
+              />
+            )}
 
-          <Sphere fill={theme.colors.background} />
-          <Raster
-            source={datasets['gcm_central-america.zarr']}
-            colormap={colormap}
-            mode={'lut'}
-            clim={[280, 310]}
-            nullValue={9.969209968386869e36}
-            variable={'tasmax'}
-          />
-        </Minimap>
+            <Sphere fill={theme.colors.background} />
+
+            <Raster
+              source={data}
+              bounds={bounds}
+              colormap={colormap}
+              mode={'lut'}
+              clim={[280, 310]}
+              nullValue={NAN}
+            />
+          </Minimap>
+        )}
       </Box>
     </>
   )
