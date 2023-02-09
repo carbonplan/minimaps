6e7d401633cc689ad67bb4d4e9bf22aa497098d5
katamartin <katasm@gmail.com>
2023-02-08 18:13:47 -0800

    WIP

--- src/path.js
+++ src/path.js
@@ -13,7 +13,7 @@ const Path = ({
 }) => {
   const [path, setPath] = useState()
   const [data, setData] = useState()
-  const { projection, width, height } = useMinimap()
+  const { projection, scale, translate, width, height } = useMinimap()
 
   useEffect(() => {
     fetch(source)
@@ -27,6 +27,21 @@ const Path = ({
     setPath(geoPath(projection)(data))
   }, [data, projection])
 
+  const actualScale = [
+    (scale * (width / (2 * Math.PI))) / projection.scale(),
+    (scale * (height / Math.PI)) / projection.scale(),
+  ]
+  let actualTranslate = [
+    ((1 + translate[0]) * width) / 2 +
+      ((projection.translate()[0] / projection.scale()) * 2) / width,
+    ((1 + translate[1]) * height) / 2 +
+      ((projection.translate()[1] / projection.scale()) * 2) / height,
+  ]
+  // actualTranslate = [20, 3]
+
+  console.log('projected', projection.scale(), projection.translate())
+  console.log('props', scale, translate)
+  console.log('calculated', actualScale, actualTranslate)
   return (
     <path
       d={path}
@@ -34,6 +49,9 @@ const Path = ({
       fill={fill}
       opacity={opacity}
       strokeWidth={strokeWidth}
+      transform={`scale(${actualScale.join(
+        ' '
+      )}) translate(${actualTranslate.join(' ')})`}
       style={{
         vectorEffect: 'non-scaling-stroke',
       }}
