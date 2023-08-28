fef859a4c0351f174796448ad24eb3b1b9d2092e
freeman-lab <the.freeman.lab@gmail.com>
2023-08-27 20:48:10 -0700

    precommit

--- projections/src/naturalEarth1.js
+++ projections/src/naturalEarth1.js
@@ -3,11 +3,11 @@ import { naturalEarth1, naturalEarth1Invert } from 'glsl-geo-projection'
 
 export default function () {
   const projection = geoNaturalEarth1().precision(0.1)
-  projection.glsl = { 
+  projection.glsl = {
     name: 'naturalEarth1Invert',
-    func: naturalEarth1Invert, 
+    func: naturalEarth1Invert,
     nameForward: 'naturalEarth1',
-    funcForward: naturalEarth1,  
+    funcForward: naturalEarth1,
   }
   projection.id = 'naturalEarth1'
   return projection
